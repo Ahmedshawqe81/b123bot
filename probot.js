446d5548b5c4aeeeb764c2948f41b168e6317909
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2019-01-27 14:43:41 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -1521,8 +1521,8 @@ if(message.content.startsWith(prefix  +  'warn'))  {
 .addField('**-  Reason:**',  `[${reason}]`,  true)
 .addField("**-  Warned  in:**",`[${message.channel.name}]`)
 .addField("**-  Time & Date:**",`[${message.createdAt}]`)
-.setFooter("Probot")
-.setColor('#060c37')
+.setFooter("Positive System")
+.setColor('RANDOM')
 let incidentchannel = message.guild.channels.find(`name`, "warns");
 if(!incidentchannel) return message.channel.send("Can't find warns channel.");
 incidentchannel.send(reportembed);
@@ -4794,33 +4794,6 @@ client.on('message', message => {
 
   });
 
-client.on('message',  async  message  =>  {
-  let  user  =  message.mentions.users.first();
-  let  reason  =  message.content.split(' ').slice(2).join(' ');
-if(message.content.startsWith(prefix  +  'warn'))  {
-  message.delete(); 
-  if(!message.member.hasPermission('MUTE_MEMBERS')) return      message.channel.send('**للأسف لا تمتلك صلاحيات' );
-  if(!user)  return  message.channel.send("**  -  mention  a  member  **")
-  if(!reason)  return  message.channel.send("**  -  Type  Reason  **")
-  let  reportembed  =  new  Discord.RichEmbed()
-  .setTitle(`**New  Warned User !**`)
-.addField("**-  Warned  User:**",  `[${user}  with  ID  ${user.id}]`)
-.addField('**-  Warned  By:**',`[${message.author.tag} with id ${message.author.id}]`)
-.addField('**-  Reason:**',  `[${reason}]`,  true)
-.addField("**-  Warned  in:**",`[${message.channel.name}]`) 
-.addField("**-  Time & Date:**",`[${message.createdAt}]`)
-.setFooter("Probot")
-.setColor('#060c37')
-let incidentchannel = message.guild.channels.find(`name`, "warns"); 
-if(!incidentchannel) return message.channel.send("Can't find warns channel."); 
-incidentchannel.send(reportembed);
-message.reply(`**:warning: ${user} has been warned !:warning:**`).then(msg  =>  msg.delete(3000));
-user.send(`**:warning: You are has been warned in ${message.guild.name} reason: ${reason} :warning:**`) 
-}
-
-
-
-})
 
 
 
