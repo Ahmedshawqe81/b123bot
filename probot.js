dee17051d50f9c9cf2b9e261ca8e661c991941a2
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2019-01-29 17:02:17 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -4912,9 +4912,9 @@ LOka.on('guildMemberAdd', member => {
 client.on('message',async message => {
 if(message.content === adminprefix + "restart") {// لعمل ريسترت للبوت
       if (!devs.includes(message.author.id)) return;
-          message.channel.send(⚠️ **Bot restarting by ${message.author.username}**);
+          message.channel.send(**Bot restarting by ${message.author.username}**);
         console.log("\n\n~~~~~~~~~~~");
-        console.log(⚠️ Bot restarting... ⚠️);
+        console.log("⚠️ Restarting Bot ⚠️");
         console.log("===============================================\n\n");
         client.destroy();
         child_process.fork(__dirname + "/bot.js");
