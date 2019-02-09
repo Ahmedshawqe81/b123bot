03bb2a03f16b176b3066680d2a49b23c89aebc33
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2019-02-09 17:37:25 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -2,17 +2,8 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 const prefix = ('!')
 
-const d = new Discord.RichEmbed()
-.setDescription(`${prefix}help-system
-${prefix}help-music
-${prefix}help-giveaway
-${prefix}help-files`)
-.setTitle('Visual Studio Codes Helper V0.1')
-.setFooter('VS V0.1 , All Rights Reserved ')
-.setColor('Random')
-
 
 client.on('message', message => {
     if (!message.content.startsWith(`${prefix} + 'help'`))
-    message.channel.send(d) 
+    message.channel.send('ww') 
 });
